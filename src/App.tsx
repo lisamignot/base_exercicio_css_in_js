cd70aaa9f0dc17d617a1b6fcf3fc130f4d98be5c
Lisa Mignot <lisammignot@gmail.com>
2024-12-04 23:37:22 -0300

    estilização modificada para styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import Header from './components/Cabecalho'
+import { ThemeProvider } from 'styled-components'
+import Cabecalho from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import EstiloGlobal, { Container } from './styles'
+import colorTheme from './theme/color'
 
 function App() {
   return (
-    <>
-      <Header />
+    <ThemeProvider theme={colorTheme}>
+      <EstiloGlobal />
+      <Cabecalho />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ src/theme/color.ts
@@ -0,0 +1,7 @@
+const colorTheme = {
+  corPrincipal: '#a7727d',
+  corSecundaria: '#f9f5e7',
+  corTexto: '#eee'
+}
+
+export default colorTheme
